cd081faa8788d0b7c66318489848c3d06f7946e1
RicardoRAraujo <ricardorodriguesmg@gmail.com>
2018-08-28 10:36:40 -0300

    Class Component

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Family from './family'
-import Member from './member'
+import ClassComponent from './classComponent'
+
 
 ReactDOM.render(
-  <Family lastName="Silva">
-    <Member name="Ricardo"/>
-    <Member name="Guilherme"/>
-    <Member name="Nilo"/>
-  </Family>
+  <ClassComponent value="Class Component"/>
   ,document.getElementById('app')
 )
